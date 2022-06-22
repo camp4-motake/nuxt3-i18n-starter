ba1c51dc53513ff6c2785c1673f35b07403fb0da
motake <munetoshi_otake@camp4.jp>
2022-06-23 08:23:14 +0900

    update

---
+++ src/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useError } from "#app";
+const error = useError();
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li>{{ error.statusCode }}</li>
+      <li>{{ error.message }}</li>
+    </ul>
+  </div>
+</template>
