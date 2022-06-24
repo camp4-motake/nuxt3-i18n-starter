99c943c929882958cc4c3c1961cb4fa6b9fe0d0a
motake <munetoshi_otake@camp4.jp>
2022-06-24 09:50:56 +0900

    add slash

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,9 @@
     <slot name="header"> Default header content </slot>
     <nav>
       <NuxtLink to="/">home</NuxtLink>
-      <NuxtLink to="/about">about</NuxtLink>
+      <NuxtLink to="/about/">about</NuxtLink>
       <NuxtLink to="/en/">en</NuxtLink>
-      <NuxtLink to="/en/about">en-about</NuxtLink>
+      <NuxtLink to="/en/about/">en-about</NuxtLink>
     </nav>
   </header>
 </template>
