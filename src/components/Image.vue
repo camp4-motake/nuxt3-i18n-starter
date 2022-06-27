51242d17a8829c103da1aa3a572471cb6dfe5d0b
motake <munetoshi_otake@camp4.jp>
2022-06-28 08:41:13 +0900

    add conponents

---
+++ src/components/Image.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface Props {
+  srcWebp: string | {};
+  srcImg: {};
+  alt: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  srcWebp: "",
+  srcImg: undefined,
+  alt: "",
+});
+</script>
+
+<template>
+  <picture>
+    <source :srcset="srcWebp" type="image/webp" />
+    <img
+      :src="srcImg.src"
+      :width="srcImg.width"
+      :height="srcImg.height"
+      :alt="alt"
+      decoding="async"
+    />
+  </picture>
+</template>
---
+++ src/components/Locale.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  from: string;
+}
+
+const { lang } = useLang();
+const props = withDefaults(defineProps<Props>(), {
+  from: "",
+});
+</script>
+
+<template>
+  <div v-if="lang === from">
+    {{ lang === from }}
+    <slot />
+  </div>
+</template>
